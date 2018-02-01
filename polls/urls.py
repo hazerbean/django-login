04e4097bd480d72af1e12e9a681339e1dc921bf5
hazerbean <up614241@port.ac.uk>
2018-02-01 10:16:18 +0000

    next

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,9 @@
-from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
+from django.urls import include, path
+
 
 from . import views
 from .forms import CustomAuthenticationForm
@@ -9,4 +14,6 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+        url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+        url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
 ]
